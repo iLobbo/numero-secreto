1c0573b00c4a96a0aa77fa7ccb949844649aac46
Luan Lima <luanlima021208@gmail.com>
2024-07-12 11:20:13 -0300

    Retornando as mudanças e logs adicionadas

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function exibirMensagemInicial() {
-exibirTextoNaTela("h1", "Jogo do Número Secreto");
+exibirTextoNaTela("h1", "Jogo do número secreto");
 exibirTextoNaTela("p", `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
